ccf4c46de84e1f314d90457cbb129a6d37db64dd
João Henrique <joaohenriquetdj@gmail.com>
2022-03-01 16:24:37 -0300

    add application and my thread

---
+++ src/com/github/joaoh4547/threads_java/Application.java
@@ -0,0 +1,10 @@
+package com.github.joaoh4547.threads_java;
+
+public class Application {
+
+	public static void main(String[] args) {
+	
+
+	}
+
+}
---
+++ src/com/github/joaoh4547/threads_java/MyThread.java
@@ -0,0 +1,9 @@
+package com.github.joaoh4547.threads_java;
+
+public class MyThread extends Thread{
+	private String name;
+	
+	public MyThread(String name) {
+		this.name = name;
+	}
+}
