7f311670e347f6d3ce5773e00b651cdcf4243361
João Henrique <joaohenriquetdj@gmail.com>
2022-03-01 16:42:59 -0300

    run threads

--- src/com/github/joaoh4547/threads_java/Application.java
+++ src/com/github/joaoh4547/threads_java/Application.java
@@ -4,7 +4,10 @@ public class Application {
 
 	public static void main(String[] args) {
 	
-
+		new MyThread("thread #1",2000);
+		new MyThread("thread #2",1200);
+		new MyThread("thread #3",500);
+		
 	}
 
 }
--- src/com/github/joaoh4547/threads_java/MyThread.java
+++ src/com/github/joaoh4547/threads_java/MyThread.java
@@ -2,8 +2,27 @@ package com.github.joaoh4547.threads_java;
 
 public class MyThread extends Thread{
 	private String name;
+	private Integer time;
 	
-	public MyThread(String name) {
+	public MyThread(String name,Integer time) {
 		this.name = name;
+		this.time = time;
+		this.start();
+	}
+	
+	@Override
+	public void run() {
+		System.out.println(String.format("Executando athread; %s", this.name));
+		
+		for(int  i=0; i<=10;i++) {
+			System.out.println(String.format("Thread %s - contador %d ",this.name, i));
+			try {
+				Thread.sleep(this.time);
+			} catch (InterruptedException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+		System.out.println(String.format("\n*** Thread [%s] finalizada ***\n",this.name ));
 	}
 }
